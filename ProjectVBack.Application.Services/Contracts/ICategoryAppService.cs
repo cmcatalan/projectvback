7fb7bbcb065c806807b4322e6c639609ed11323f
Wylli <17494657+cmcatalan@users.noreply.github.com>
2022-05-03 13:20:14 +0200

    Add UserController and GetUserInfo closes #49

--- ProjectVBack.Application.Services/Contracts/ICategoryAppService.cs
+++ ProjectVBack.Application.Services/Contracts/ICategoryAppService.cs
@@ -2,5 +2,6 @@
 {
     public interface ICategoryAppService
     {
+        Task<bool> CreateCategoryAsync();
     }
 }
--- ProjectVBack/Controllers/CategoriesController.cs
+++ ProjectVBack/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjectVBack.Application.Services;
 
 namespace ProjectVBack.Controllers
 {
@@ -7,11 +8,15 @@ namespace ProjectVBack.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ILogger<CategoriesController> _logger;
+        private readonly ICategoryAppService _categoryAppService;
 
-        public CategoriesController(ILogger<CategoriesController> logger)
+        public CategoriesController(ILogger<CategoriesController> logger , ICategoryAppService categoryAppService)
         {
             _logger = logger;
+            _categoryAppService = categoryAppService;
         }
+
+
 
         //TODO Get all categories only by user logged, user_id from usermanager
         //TODO Get category by id only by user logged, user_id from usermanager
