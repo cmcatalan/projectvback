45fb3cbe0a5324a80020575b89acc2a431630300
Wylli <17494657+cmcatalan@users.noreply.github.com>
2022-05-10 18:44:26 +0200

    Added 3 unit tests of CategoryAppService

--- ProjectVBack.Testing.UnitTesting/Implementations/CategoryAppServiceTests.cs
+++ ProjectVBack.Testing.UnitTesting/Implementations/CategoryAppServiceTests.cs
@@ -16,6 +16,8 @@ using System.Security.Claims;
 using ProjectVBack.Application.Dtos.CategoryService;
 using ProjectVBack.Crosscutting.Utils;
 using ProjectVBack.Crosscutting.CustomExceptions;
+using System.Threading;
+using FluentValidation.Internal;
 
 namespace ProjectVBack.Application.Services.Tests
 {
@@ -35,80 +37,31 @@ namespace ProjectVBack.Application.Services.Tests
             _userManagerMock = new Mock<UserManager<User>>(_userStoreMock.Object, null, null, null, null, null, null, null, null);
             _userManagerMock.Object.UserValidators.Add(new UserValidator<User>());
             _userManagerMock.Object.PasswordValidators.Add(new PasswordValidator<User>());
-            _userManagerMock.Setup(f => f.GetUserAsync(It.IsAny<ClaimsPrincipal>()))
-                .Returns(Task.FromResult(new User()
-                {
-                    FirstName = "Guillermo",
-                    LastName = "Cuenca",
-                    Categories = new List<Category>(),
-                    Transactions = new List<Transaction>()
-                }));
-            _userManagerMock.Setup(f => f.FindByIdAsync(It.IsAny<string>()))
-                .Returns(Task.FromResult(new User()
-                {
-                    FirstName = "Guillermo",
-                    LastName = "Cuenca",
-                    Categories = new List<Category>(),
-                    Transactions = new List<Transaction>()
-                }));
+
 
             _unitOfWorkMock = new Mock<IUnitOfWork>();
             _unitOfWorkMock.Setup(f => f.Categories.AddAsync(It.IsAny<Category>()))
-                .Returns(Task.FromResult(new Category()
-                {
-                    Id = 1,
-                    Name = "Esports",
-                    PictureUrl = "websot.url",
-                    Description = "Aqui me dejo los euros pa G2",
-                    IsDefault = true,
-                    Users = new List<User>(),
-                    Transactions = new List<Transaction>()
-                }));
+                .Returns(Task.FromResult(FixtureDataCategory.category));
             _unitOfWorkMock.Setup(f => f.Complete()).Verifiable();
 
             _mapperMock = new Mock<IMapper>();
             _mapperMock.Setup(f => f.Map<Category>(It.IsAny<CategoryDto>()))
-                .Returns(new Category()
-                {
-                    Id = 1,
-                    Name = "Esports",
-                    PictureUrl = "websot.url",
-                    Description = "Aqui me dejo los euros pa G2",
-                    IsDefault = true,
-                    Users = new List<User>(),
-                    Transactions = new List<Transaction>()
-                });
+                .Returns(FixtureDataCategory.category);
             _mapperMock.Setup(f => f.Map<CategoryDto>(It.IsAny<Category>()))
-                .Returns(new CategoryDto()
-                {
-                    Type = CategoryType.Income,
-                    Id = 1,
-                    Name = "Esports",
-                    PictureUrl = "websot.url",
-                    Description = "Aqui me dejo los euros pa G2",
-                    IsDefault = true,
-                });
+                .Returns(FixtureDataCategory.categoryDto);
             _mapperMock.Setup(f => f.Map<Category>(It.IsAny<AddCategoryRequest>()))
-                .Returns(new Category()
-                {
-                    Type = CategoryType.Income,
-                    Id = 1,
-                    Name = "Esports",
-                    PictureUrl = "websot.url",
-                    Description = "Aqui me dejo los euros pa G2",
-                    IsDefault = true,
-                });
+                .Returns(FixtureDataCategory.category);
 
 
-            var errorList = new List<FluentValidation.Results.ValidationFailure>();
+
 
             _editCategoryRequestValidatorMock = new Mock<IValidator<EditCategoryRequest>>();
-            _editCategoryRequestValidatorMock.Setup(f => f.Validate(It.IsAny<EditCategoryRequest>()))
-                .Returns(new FluentValidation.Results.ValidationResult(errorList));
+            _editCategoryRequestValidatorMock.Setup(x => x.ValidateAsync(It.IsAny<EditCategoryRequest>(), It.IsAny<CancellationToken>()))
+                             .ReturnsAsync(FixtureDataCategory.validationResult);
 
             _addCategoryRequestValidatorMock = new Mock<IValidator<AddCategoryRequest>>();
-            _addCategoryRequestValidatorMock.Setup(f => f.Validate(It.IsAny<AddCategoryRequest>()))
-                .Returns(new FluentValidation.Results.ValidationResult(errorList));
+            _addCategoryRequestValidatorMock.Setup(x => x.ValidateAsync(It.IsAny<AddCategoryRequest>(), It.IsAny<CancellationToken>()))
+                             .ReturnsAsync(FixtureDataCategory.validationResult);
         }
 
         [TestMethod()]
@@ -118,28 +71,29 @@ namespace ProjectVBack.Application.Services.Tests
         }
 
         [TestMethod()]
-        public void CreateCategoryAsyncTest_GoodPath()
+        public async Task Create_Category_Async_Test_Happy_Path()
         {
+            _unitOfWorkMock.Setup(f => f.Categories.AddAsync(It.IsAny<Category>())).Returns(Task.FromResult(FixtureDataCategory.category));
+            _userManagerMock.Setup(f => f.FindByIdAsync(FixtureDataCategory.userId)).Returns(Task.FromResult(FixtureDataCategory.user));
+
             var categoryAppService = new CategoryAppService(_unitOfWorkMock.Object, _userManagerMock.Object, _mapperMock.Object,
                 _editCategoryRequestValidatorMock.Object, _addCategoryRequestValidatorMock.Object);
 
-            var result = categoryAppService.CreateCategoryAsync(new AddCategoryRequest("Esports", "Antonio", "AntonioLopez", CategoryType.Expense), "vbnmkl").Result;
+            var result = await categoryAppService.CreateCategoryAsync(FixtureDataCategory.addCategoryRequest, FixtureDataCategory.userId);
 
             Assert.IsNotNull(result);
 
-            Assert.IsInstanceOfType(result , typeof(CategoryDto));
+            Assert.IsInstanceOfType(result, typeof(CategoryDto));
         }
 
-        [TestMethod()]
-        [ExpectedException(typeof(AppIGetMoneyInvalidUserException))]
-        public void CreateCategoryAsyncTest_BadPath()
+        [TestMethod]
+        [ExpectedException(typeof(AppIGetMoneyUserNotFoundException))]
+        public async Task Create_Category_Async_Test_BadPath_User_Not_Found()
         {
-            
-
             var categoryAppService = new CategoryAppService(_unitOfWorkMock.Object, _userManagerMock.Object, _mapperMock.Object,
                 _editCategoryRequestValidatorMock.Object, _addCategoryRequestValidatorMock.Object);
 
-            var result = categoryAppService.CreateCategoryAsync(new AddCategoryRequest("Esports", "Antonio", "AntonioLopez", CategoryType.Expense), "vbnmkl").Result;
+           await categoryAppService.CreateCategoryAsync(FixtureDataCategory.addCategoryRequest, FixtureDataCategory.userId);
         }
 
         [TestMethod()]
@@ -155,9 +109,37 @@ namespace ProjectVBack.Application.Services.Tests
         }
 
         [TestMethod()]
-        public void EditCategoryAsyncTest()
+        public async Task EditCategoryAsyncTest()
         {
-            Assert.Fail();
+            Category categoryTest = new Category()
+            {
+                Type = CategoryType.Expense,
+                Name = "Travelling",
+                PictureUrl = "thispictureUrl",
+                Description = "This category is for the traveling expended money",
+                IsDefault = false,
+                Users = new List<User>()
+                {
+                    FixtureDataCategory.user
+                },
+                Transactions = new List<Transaction>(),
+            };
+
+            _unitOfWorkMock.Setup(f => f.Categories.GetCategoryWithUsersByIdAsync(FixtureDataCategory.categoryId))
+                .Returns(Task.FromResult((Category ?)categoryTest));
+            _unitOfWorkMock.Setup(f => f.Categories.UpdateAsync(FixtureDataCategory.category))
+                .Returns(Task.FromResult((Category?)FixtureDataCategory.category));
+            _userManagerMock.Setup(f => f.FindByIdAsync(FixtureDataCategory.userId))
+                .Returns(Task.FromResult(FixtureDataCategory.user));
+
+            var categoryAppService = new CategoryAppService(_unitOfWorkMock.Object, _userManagerMock.Object, _mapperMock.Object,
+                _editCategoryRequestValidatorMock.Object, _addCategoryRequestValidatorMock.Object);
+
+            var result = await categoryAppService.EditCategoryAsync(FixtureDataCategory.editCategoryRequest , FixtureDataCategory.userId);
+
+            Assert.IsNotNull(result);
+
+            Assert.IsInstanceOfType(result, typeof(CategoryDto));
         }
 
         [TestMethod()]
---
+++ ProjectVBack.Testing.UnitTesting/Implementations/FixtureDataCategory.cs
@@ -0,0 +1,50 @@
+﻿using ProjectVBack.Application.Dtos;
+using ProjectVBack.Application.Dtos.CategoryService;
+using ProjectVBack.Crosscutting.Utils;
+using ProjectVBack.Domain.Entities;
+using System.Collections.Generic;
+
+public static class FixtureDataCategory
+{
+    public static string userId = "1";
+    public static int categoryId = 1;
+
+    public static Category category = new Category
+    {
+        Type = CategoryType.Expense,
+        Name = "Travelling",
+        PictureUrl = "thispictureUrl",
+        Description = "This category is for the traveling expended money",
+        IsDefault = false,
+        Users = new List<User>(),
+        Transactions = new List<Transaction>(),
+    };
+
+    public static CategoryDto categoryDto = new CategoryDto
+    {
+        Type = CategoryType.Expense,
+        Name = "Travelling",
+        PictureUrl = "thispictureUrl",
+        Description = "This category is for the traveling expended money",
+        IsDefault = false,
+        Id = 5
+    };
+
+    public static User user = new User
+    {
+        FirstName = "Guillermo",
+        LastName = "Cuenca",
+        Categories = new List<Category>(),
+        Transactions = new List<Transaction>()
+    };
+
+    public static AddCategoryRequest addCategoryRequest = new AddCategoryRequest("Travelling", "thispictureUrl",
+        "This category is for the traveling expended money", CategoryType.Expense);
+
+    public static EditCategoryRequest editCategoryRequest = new EditCategoryRequest("Travelling", "thispictureUrl",
+    "This category is for the traveling expended money", CategoryType.Expense , 1);
+
+    public static IEnumerable<FluentValidation.Results.ValidationFailure> errorList = new List<FluentValidation.Results.ValidationFailure>();
+
+    public static FluentValidation.Results.ValidationResult validationResult = new FluentValidation.Results.ValidationResult();
+}
